e7ecd45ccd017a94b0385a74f83d987dabc5a0e5
oikkoikk <ljjun0702@gmail.com>
2025-04-19 00:17:53 +0900

    feat: Add react-router

--- src/application/apps/App.tsx
+++ src/application/apps/App.tsx
@@ -1,3 +1,11 @@
+import { Route, Routes } from 'react-router';
+
+import { HomePage } from '@/presentation/pages/pages';
+
 export function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
+
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { App } from '@application/apps/apps';
+import { BrowserRouter } from 'react-router';
+
+import { App } from '@/application/apps/apps';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
