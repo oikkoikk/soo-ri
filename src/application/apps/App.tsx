d1e96f80a6150230635801ec53881c7194bdd15a
oikkoikk <ljjun0702@gmail.com>
2025-04-21 21:28:56 +0900

    feat: Add GlobalStyle

--- src/application/apps/App.tsx
+++ src/application/apps/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from '@emotion/react'
 import { BrowserRouter } from 'react-router'
 
 import { Router } from '@/application/routers/routers'
+import { GlobalStyle } from '@/theme/styles/GlobalStyle'
+import { theme } from '@/theme/theme'
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
---
+++ src/theme/styles/GlobalStyle.tsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global } from '@emotion/react'
+import '../fonts/fonts.css'
+
+export function GlobalStyle() {
+  return (
+    <Global
+      styles={css`
+        /* RESET */
+        *,
+        *::before,
+        *::after {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+
+        html,
+        body {
+          width: 100%;
+          height: 100%;
+          font-family:
+            'Pretendard Variable',
+            Pretendard,
+            -apple-system,
+            BlinkMacSystemFont,
+            'Segoe UI',
+            Roboto,
+            'Helvetica Neue',
+            sans-serif;
+          background-color: #ffffff;
+          color: #000000;
+        }
+
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
+
+        button {
+          all: unset;
+          cursor: pointer;
+        }
+
+        ul,
+        ol {
+          list-style: none;
+        }
+
+        img {
+          max-width: 100%;
+          display: block;
+        }
+      `}
+    />
+  )
+}
