8a4bca4d248ee2309c826f4db140a3d3ae73fd70
Lee, Jongjun <ljjun0702@gmail.com>
2025-04-21 22:07:55 +0900

    feat: Add theme (#4)
    * feat: Add Emotion
    
    * feat: Add theme
    
    * feat: Add GlobalStyle
    
    * fix: Remove @emotion/styled
    
    * feat: Add theme barrel file

--- src/application/apps/App.tsx
+++ src/application/apps/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from '@emotion/react'
 import { BrowserRouter } from 'react-router'
 
 import { Router } from '@/application/routers/routers'
+import { GlobalStyle } from '@/theme/styles/styles'
+import { SOORITheme } from '@/theme/theme'
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={SOORITheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
