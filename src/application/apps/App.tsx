dfb2b743381cfb23f2b25a651865862251668cbf
oikkoikk <ljjun0702@gmail.com>
2025-05-11 01:07:05 +0900

    feat: Add ProgressIndicator with react portal

--- src/application/apps/App.tsx
+++ src/application/apps/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from '@emotion/react'
 import { RouterProvider } from 'react-router'
 
+import { ProgressIndicator } from '@/presentation/components/components'
 import { SOORITheme, GlobalStyle } from '@/theme/theme'
 
 import { FirebaseProvider } from '../configurations/configurations'
+import { LoadingProvider, useLoading } from '../configurations/contexts/contexts'
 import { router } from '../routers/routers'
 
 export function App() {
@@ -11,8 +13,21 @@ export function App() {
     <ThemeProvider theme={SOORITheme}>
       <GlobalStyle />
       <FirebaseProvider>
-        <RouterProvider router={router} />
+        <LoadingProvider>
+          <AppContent />
+        </LoadingProvider>
       </FirebaseProvider>
     </ThemeProvider>
+  )
+}
+
+export function AppContent() {
+  const { loading } = useLoading()
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      {loading && <ProgressIndicator />}
+    </>
   )
 }
--- src/presentation/components/components.ts
+++ src/presentation/components/components.ts
@@ -1,2 +1,3 @@
 export { Header } from './Header'
+export { ProgressIndicator } from './ProgressIndicator'
 export { Tabs } from './Tabs'
