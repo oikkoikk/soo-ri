33a45fb4930eab42e6ce989f9e121509d2a3a4db
oikkoikk <ljjun0702@gmail.com>
2025-05-15 01:12:07 +0900

    feat: Add VITE_DISCORD_WEBHOOK_URL to environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,6 +10,7 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_APP_ID: string
   readonly VITE_FIREBASE_MEASUREMENT_ID: string
   readonly VITE_SOORI_BASE_URL: string
+  readonly VITE_DISCORD_WEBHOOK_URL: string
 }
 
 interface ImportMeta {
