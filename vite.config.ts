7b10c8a3c639d45f860f2e99eb2a232cf2da5949
oikkoikk <ljjun0702@gmail.com>
2025-06-21 09:36:37 +0900

    fix: Add vite-plugin-remove-console for build-time console log removal

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import removeConsole from 'vite-plugin-remove-console'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), svgr(), command === 'build' && removeConsole({ includes: ['log', 'warn', 'error'] })],
+    resolve: {
+      alias: {
+        '@': '/src',
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  }
 })
