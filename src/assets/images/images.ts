4a9f97ec61d18c0948b5a84ead8a36b714ac748c
oikkoikk <ljjun0702@gmail.com>
2025-06-23 19:31:00 +0900

    feat: Add partner wordmark images

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,1 +1,4 @@
-export { default as WordMark } from './wordmark.png'
+export { default as Wordmark } from './wordmark.png'
+export { default as KaKaoImpactWordmark } from './kakao-impact-wordmark.png'
+export { default as SDCommunityWelfareCenterWordmark } from './sd-community-welfare-center-wordmark.png'
+export { default as StarPickersWordmark } from './starpickers-wordmark.png'
